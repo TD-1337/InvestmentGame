eb8fba2b71c5c6b163a06045c878de765a909281
NL\TvanDongen <tvandongen@deloitte.nl>
2019-09-24 13:57:15 +0200

    added test

--- Stock.py
+++ Stock.py
@@ -1,3 +1,7 @@
 class Stock:
-    def __init__(self, name):
+    def __init__(self, name, stock_price_hist):
         self.name = name
+        self.stock_price_hist = stock_price_hist
+
+    def retrieve_stock_price(self):
+        
