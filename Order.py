41047903052367f6ab590c7e5745bc2ecc17924f
sharo <sharoalfaro@deloitte.nl>
2019-09-24 15:26:22 +0200

    changed main

--- Order.py
+++ Order.py
@@ -1,7 +1,5 @@
 class Order:
 
-    from datetime import datetime
-
     def __init__(self, stock, volume, dateOfPurchase, orderId):
 
         self.Stock = stock
@@ -10,4 +8,4 @@ class Order:
         self.OrderId = orderId
 
         print('You bought ', self.Volume, ' number of ', self.Stock, ' stocks, at ', self.DateOfPurchase, '. OrderID= ',
-              self.OrderID, sep='')
+              self.OrderId, sep='')
