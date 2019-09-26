6d832a537b97faada7d976ba44b66ee41a77f663
TimWolterink <t_wolterink@hotmail.com>
2019-09-26 13:02:57 +0200

    sell order added to view portfolio

--- NewMain.py
+++ NewMain.py
@@ -61,36 +61,36 @@ def buy_order():
         return render_template("buy_order.html")
 
 
-#Sell order page
-@app.route('/sell_order', methods=['POST','GET'])
-def sell_order():
-    if request.method == "POST":
-
-        number_of_existing_orders = sum([len(portfolio.orders) for portfolio in portfolio_dict.values()])
-
-        # User input data
-        portfolio_name = request.form['portfolio']
-        stock = request.form['stock']
-        amount = request.form['amount']
-        next_action = request.form['next_action']
-
-        #add order to dictionary
-        portfolio_this_order = determine_portfolio(portfolio_name, portfolio_dict)
-        new_order = create_sell_order(stock,amount,number_of_existing_orders,portfolio_this_order)
-        portfolio_this_order.add_order(new_order)
-
-        #do something with this input
-        if next_action =="No":
-            Exporter.export_to_csv(portfolio_dict)
-            return "Thank you for your business, you have sold "+amount+" of "+stock+" stocks in portfolio "+portfolio_name
-            # Export to CSV
-        elif next_action =="Yes":
-            return  render_template("sell_order.html")
-        elif next_action =="Back to Home":
-            Exporter.export_to_csv(portfolio_dict)
-            return redirect("/")
-    else:
-        return render_template("sell_order.html")
+# #Sell order page
+# @app.route('/sell_order', methods=['POST','GET'])
+# def sell_order():
+#     if request.method == "POST":
+#
+#         number_of_existing_orders = sum([len(portfolio.orders) for portfolio in portfolio_dict.values()])
+#
+#         # User input data
+#         portfolio_name = request.form['portfolio']
+#         stock = request.form['stock']
+#         amount = request.form['amount']
+#         next_action = request.form['next_action']
+#
+#         #add order to dictionary
+#         portfolio_this_order = determine_portfolio(portfolio_name, portfolio_dict)
+#         new_order = create_sell_order(stock,amount,number_of_existing_orders,portfolio_this_order)
+#         portfolio_this_order.add_order(new_order)
+#
+#         #do something with this input
+#         if next_action =="No":
+#             Exporter.export_to_csv(portfolio_dict)
+#             return "Thank you for your business, you have sold "+amount+" of "+stock+" stocks in portfolio "+portfolio_name
+#             # Export to CSV
+#         elif next_action =="Yes":
+#             return  render_template("sell_order.html")
+#         elif next_action =="Back to Home":
+#             Exporter.export_to_csv(portfolio_dict)
+#             return redirect("/")
+#     else:
+#         return render_template("sell_order.html")
 
 #Select Portfolio Page
 @app.route('/select_portfolio', methods=['POST','GET'])
@@ -98,17 +98,32 @@ def select_and_view_portfolio():
     if request.method == "POST":
         #process user data
         portfolio_name = request.form['portfolio']
-        portfolio_df = sub_portfolio(portfolio_name)
-
         return render_template("view_portfolio.html",  portfolio= portfolio_dict[portfolio_name])
     else:
         return render_template("select_portfolio.html")
 
+#Select Portfolio Page
+@app.route('/sell_order2', methods=['POST','GET'])
+def sell_order():
+    if request.method == "POST":
+        #process user data
+        portfolio_name = request.form['portfolio']
+        stock = request.form['stock']
+        amount = request.form['amount']
+        next_action = request.form['next_action']
 
-def sub_portfolio(portfolio_name):
-        print_report_df = pd.DataFrame([x.return_as_dict() for x in portfolio_dict[portfolio_name].orders])
-        #print("Your portfolio consists of the following: ")
-        #print(print_report_df)  # Add optionality
-        return print_report_df
+        # Next action
+        if next_action == "No":
+            Exporter.export_to_csv(portfolio_dict)
+            return "Thank you for your business, you have sold " + amount + " of " + stock + " stocks in portfolio " + portfolio_name
+            # Export to CSV
+        elif next_action == "Yes":
+            return render_template("sell_order2.html")
+        elif next_action == "Back to Home":
+            Exporter.export_to_csv(portfolio_dict)
+            return redirect("/")
+    else:
+        return render_template("sell_order2.html")
+
 
 
